77530a988e592343b00cfe528c84e0eebdbc76ec
kazewong <kazewong.physics@gmail.com>
2024-08-14 16:11:45 -0400

    update pytoml and sort

---
+++ src/insertion_sort/sort.py
@@ -0,0 +1,11 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
+            j -= 1
+    return arr
+
+def run_sort():
+    arr = [12, 11, 13, 5, 6]
+    print(insertion_sort(arr))
