79ec95b7ba581d1179bfebf466780265279c8ad8
Kaze Wong <kazewong.physics@gmail.com>
2024-08-14 15:17:35 -0400

    Update test_sort.py

--- test_sort.py
+++ test_sort.py
@@ -1,9 +1,6 @@
 import random
 import time
 
-N = 100
-random.seed(int(time.time()))
-
 def insertion_sort(unsorted_list: list[int]) -> list[int]:
     # Implement your function here ...
     raise NotImplementedError
@@ -14,9 +11,16 @@ def test_insertion_sort():
     random.shuffle(input_list)
     output_list = insertion_sort(input_list)
     assert output_list == sorted(output_list), "Input list is not sorted"
+    print("Test passed!")
 
 def test_reference():
     input_list = list(range(N))
     random.shuffle(input_list)
     output_list = sorted(input_list)
     assert output_list == sorted(output_list), "Input list is not sorted"
+    print("Test passed!")
+
+if __name__ == __main__:
+    N = 100
+    random.seed(int(time.time()))
+    test_insertion_sort()
